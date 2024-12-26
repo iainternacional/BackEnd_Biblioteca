6c12a33cad2b53762942a3fcb2afadbc125437e5
Andres Puerta <andres-felipe027@hotmail.com>
2024-12-26 12:40:39 -0500

    Crear capa 3 y 4

---
+++ Biblioteca_BackEnd/Biblioteca.BackEnd.Domain.Entities/Biblioteca/Autor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.BackEnd.Domain.Entities.Biblioteca
+{
+    public class Autor
+    {
+        public string? NombreCompleto { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string? CiudadProcedencia { get; set; }
+        public string? CorreoElectronico { get; set; }
+    }
+}
---
+++ Biblioteca_BackEnd/Biblioteca.BackEnd.Domain.Entities/Biblioteca/Libro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.BackEnd.Domain.Entities.Biblioteca
+{
+    public class Libro
+    {
+        public string? Titulo { get; set; }
+        public int Anio { get; set; }
+        public string? Genero { get; set; }
+        public int NumeroPaginas { get; set; }
+
+        public Autor? Autor { get; set; }
+    }
+}
